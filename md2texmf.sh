1b062eda7203a9b7a5dddc026b326cd44b8a34f1
ugroh <100772767+ugroh@users.noreply.github.com>
2022-10-19 09:34:28 +0200

    Clean-Up

--- md2texmf.sh
+++ md2texmf.sh
@@ -6,6 +6,11 @@ mkdir -p ~/Library/texmf/scripts/markdown
 mkdir -p ~/Library/texmf/tex/generic/markdown 
 mkdir -p ~/Library/texmf/tex/context/third/markdown
 
+make base
+cd ./libraries
+make
+cd ..
+
 cp ./markdown.lua 						~/Library/texmf/tex/luatex/markdown
 cp ./libraries/markdown-tinyyaml.lua 	~/Library/texmf/tex/luatex/markdown
 cp ./markdown-cli.lua 					~/Library/texmf/scripts/markdown
@@ -13,4 +18,9 @@ cp ./markdown.tex						~/Library/texmf/tex/generic/markdown
 cp *.sty								~/Library/texmf/tex/latex/markdown
 cp ./t-markdown.tex					~/Library/texmf/tex/context/third/markdown
 
-
+rm ./markdown.lua 						
+rm ./libraries/markdown-tinyyaml.lua 
+rm ./markdown-cli.lua 					
+rm ./markdown.tex								
+rm *.sty								
+rm ./t-markdown.tex					
