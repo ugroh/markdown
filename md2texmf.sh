75bd39dde10d35ed848903e57f559a8415a10cbd
ugroh <100772767+ugroh@users.noreply.github.com>
2022-10-19 09:19:45 +0200

    Create md2texmf.sh
    Installation der notwendigen Files

---
+++ md2texmf.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+mkdir -p ~/Library/texmf/tex/luatex/markdown
+mkdir -p ~/Library/texmf/tex/latex/markdown
+mkdir -p ~/Library/texmf/scripts/markdown
+mkdir -p ~/Library/texmf/tex/generic/markdown 
+mkdir -p ~/Library/texmf/tex/context/third/markdown
+
+cp ./markdown.lua 						~/Library/texmf/tex/luatex/markdown
+cp ./libraries/markdown-tinyyaml.lua 	~/Library/texmf/tex/luatex/markdown
+cp ./markdown-cli.lua 					~/Library/texmf/scripts/markdown
+cp ./markdown.tex						~/Library/texmf/tex/generic/markdown 		
+cp *.sty								~/Library/texmf/tex/latex/markdown
+cp ./t-markdown.t					~/Library/texmf/tex/context/third/markdown
+
+
